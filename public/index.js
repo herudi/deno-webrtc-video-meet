f68c43b4508b823990ac576e1a43ca403d4fdba9
herudi <herudi7@gmail.com>
2022-05-20 22:05:39 +0700

    van version

--- public/index.js
+++ public/index.js
@@ -1,13 +1,15 @@
-new VanRouter({
-  render: (elem) => {
-    document.getElementById("app").innerHTML = elem;
-  },
-})
-  .use((ctx, next) => {
-    ctx.baseUrl = location.origin;
-    next();
+addEventListener("load", () => {
+  new VanRouter({
+    render: (elem) => {
+      document.getElementById("app").innerHTML = elem;
+    },
   })
-  .add("/", ({ lazy }) => lazy("/page/login.js"))
-  .add("/meet", ({ lazy }) => lazy("/page/meet.js"))
-  .add("*", () => `<h1>404 NOT FOUND</h1>`)
-  .resolve();
+    .use((ctx, next) => {
+      ctx.baseUrl = location.origin;
+      next();
+    })
+    .add("/", ({ lazy }) => lazy("/page/login.js"))
+    .add("/meet", ({ lazy }) => lazy("/page/meet.js"))
+    .add("*", () => `<h1>404 NOT FOUND</h1>`)
+    .resolve();  
+})
