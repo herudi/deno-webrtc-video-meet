b955f0248d336eff212a52b6aa27adf171f83e0e
herudi <herudi7@gmail.com>
2023-03-15 16:16:48 +0700

    1.2.6

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
-export { HttpError, nhttp } from "https://deno.land/x/nhttp@1.2.5/mod.ts";
-export type { Handler } from "https://deno.land/x/nhttp@1.2.5/mod.ts";
-export { serveStatic } from "https://deno.land/x/nhttp@1.2.5/lib/serve-static.ts";
+export { HttpError, nhttp } from "https://deno.land/x/nhttp@1.2.6/mod.ts";
+export type { Handler } from "https://deno.land/x/nhttp@1.2.6/mod.ts";
+export { serveStatic } from "https://deno.land/x/nhttp@1.2.6/lib/serve-static.ts";
 export {
   Helmet,
   n,
   renderToHtml,
-} from "https://deno.land/x/nhttp@1.2.5/lib/jsx.ts";
-export type { FC } from "https://deno.land/x/nhttp@1.2.5/lib/jsx.ts";
+} from "https://deno.land/x/nhttp@1.2.6/lib/jsx.ts";
+export type { FC } from "https://deno.land/x/nhttp@1.2.6/lib/jsx.ts";
